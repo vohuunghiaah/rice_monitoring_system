ebd96736f76c04c852f9f1db4123d00d2cef700d
vohuunghiaah <vohuunghiaah@gmail.com>
2026-04-30 00:45:53 +0700

    Data Ingestion

--- src/io/raster_wrapper.py
+++ src/io/raster_wrapper.py
@@ -1,5 +1,18 @@
+from numpy._core.defchararray import index
 import rasterio
 
-class RasterFile:
-    def __init__(seft, file_path: str):
+class RasterWrapper:
+    def __init__(self, file_path: str):
+        self.src = rasterio.open(file_path)
+        self.meta = self.src.meta
+
+    def read_band(self, band_index: int):
+        return self.src.read(band_index)
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.src.close()
+        
         
